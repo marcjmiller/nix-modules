fea4efa2223f7dea4e481af26d36e76a17629e58
Marc Miller <marc.miller@secondfront.com>
2025-10-15 13:45:42 -0400

    remove gestures key from hyprland.settings

--- hm/wms/hypr/hyprland.nix
+++ hm/wms/hypr/hyprland.nix
@@ -230,13 +230,6 @@ in
               indicator_height = 1;
             };
           };
-          gestures = {
-            workspace_swipe = true;
-            workspace_swipe_fingers = 3;
-            workspace_swipe_invert = false;
-            workspace_swipe_distance = 200;
-            workspace_swipe_forever = true;
-          };
 
           misc = {
             animate_manual_resizes = true;
